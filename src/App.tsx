d4abe33c25a139d43c31dbc6cfb6a2cee729cad6
dltmdwo05 <dltmdwo05@naver.com>
2023-03-04 16:04:37 +0900

    #3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import styled from 'styled-components';
-import Circle from './Circle';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('hello', value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="안녕ㅋ" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+      </form>
     </div>
   );
 }
