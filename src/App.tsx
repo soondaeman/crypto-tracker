c527c82da8b9a8c1111b24b1b3f34519d56e08a0
dltmdwo05 <dltmdwo05@naver.com>
2023-03-04 11:09:21 +0900

    Optional props, default value

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import styled from 'styled-components';
 import Circle from './Circle';
 
 function App() {
-  return <div>
-    <Circle />
-  </div>;
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="yellow" />
+      <Circle bgColor="tomato" text="안녕ㅋ" />
+    </div>
+  );
 }
 
 export default App;
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,10 +1,33 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Container = styled.div`
+//Container 컴포넌트의 prop 타입 지정
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+//styled component
+const Container = styled.div<ContainerProps>`
+  height: 200px;
+  width: 200px;
+  border-radius: 100px;
+  background-color: ${(prop) => prop.bgColor};
+  border: 2px solid ${(prop) => prop.borderColor};
 `;
 
-function Circle() {
-  return <Container />;
+//Circle 컴포넌트의 prop 타입 지정
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor, text = 'default text' }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
 export default Circle;
