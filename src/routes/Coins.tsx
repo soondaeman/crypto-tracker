990de59adda313335df44a0bfaf6c5049a61dd56
dltmdwo05 <dltmdwo05@naver.com>
2023-03-06 16:20:20 +0900

    #5.4 Route States

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -25,9 +25,10 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -44,6 +45,12 @@ const Title = styled.h1`
 const Loader = styled.div`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 15px;
 `;
 
 interface CoinInterface {
@@ -80,7 +87,17 @@ function Coins() {
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                />
+                ({coin.symbol}){coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
