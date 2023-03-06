10c62903f9031e45a175688c7c19af68e7423c4f
dltmdwo05 <dltmdwo05@naver.com>
2023-03-06 10:48:00 +0900

    #5.1 Styles

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(prop) => prop.theme.accentColor};
+`;
+
 function Coins() {
-  return <h1>Coins</h1>;
+  return <Title>COIN</Title>;
 }
 
 export default Coins;
