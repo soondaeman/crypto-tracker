1728514a1cc952b042698bd291fba7233264178d
dltmdwo05 <dltmdwo05@naver.com>
2023-03-06 09:22:05 +0900

    crypto-track start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(prop) => prop.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(prop) => prop.theme.textColor};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <H1>Hello</H1>
+    </Container>
   );
 }
 
