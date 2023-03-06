6c9735d74cdb6fe7d1b66ddd14c432772a1d6f33
dltmdwo05 <dltmdwo05@naver.com>
2023-03-06 09:54:24 +0900

    #5.0 Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(prop) => prop.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(prop) => prop.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-  return (
-    <Container>
-      <H1>Hello</H1>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
